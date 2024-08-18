b97c7937ee0f72a77d1a5c885964ce0dd7c266ac
Varun Mathiyalagan <140169607+Varun2773@users.noreply.github.com>
2024-08-18 22:27:45 +0530

    backend for boo store is completed

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURl = "mongodb+srv://root:27072003@bookstore.cau4n.mongodb.net/bppks-collection?retryWrites=true&w=majority&appName=BookStore";
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,41 @@
-import express, { response } from "express";
-import {PORT} from "./config.js";
+import express, { request, response } from "express";
+import { PORT, mongoDBURl } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
-app.get("/",(request,response)=>{
-    console.log(request);
-    return response.status(234).send("welcome to mern stack ");
-})
+//midleware for parsing request body
+app.use(express.json());
 
-app.listen(PORT,()=>{
-    console.log(`App is listening to port: ${PORT}`)
-})
+//midlewware for handling CORS POLICY
+//option 1
+// app.use(cors());
+//option 2
+app.use(
+  cors({
+    origin: "http://localhost:5555",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["content-type"],
+  })
+);
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("welcome to mern stack ");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDBURl)
+  .then(() => {
+    console.log("app is connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
